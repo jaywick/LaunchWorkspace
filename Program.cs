e0a51026ca86a91be9daec6c67343ef8adba4789
Jay Wick <developer@jay-wick.com>
2014-08-30 22:51:37 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaunchWorkspace
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            var args = Environment.GetCommandLineArgs().Skip(1).ToList();
+
+            if (!args.Any())
+                return;
+
+            args.ForEach((item) => Process.Start(item));
+        }
+    }
+}
